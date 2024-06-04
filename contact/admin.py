6904ffb4c7b45a15064e47bf5ad8862b128f9c37
guugimeness <gucrome1@gmail.com>
2024-06-04 16:08:19 -0300

    feat: first models + admin area config

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.apps import apps
+from contact import models
 
-# Register your models here.
+# Using decorator
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'category',
+    ordering = '-id',
+    list_filter = 'creation_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 100
+    list_editable = 'first_name', 'last_name'
+    list_display_links = 'id',
+
+
+# Register all the models
+models = apps.get_models()
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
