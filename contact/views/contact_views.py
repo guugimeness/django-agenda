efade0c480b41ce769eb90627fd16ed613290901
guugimeness <gucrome1@gmail.com>
2024-06-09 16:50:03 -0300

    feat: header + search page

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 
@@ -6,7 +7,36 @@ def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
     
     context = {
-        'contacts': contacts    
+        'contacts': contacts,
+        'site_title': 'Contatos - '  
+    }
+    
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+def search(request):
+    
+    search_value = request.GET.get('q', '').strip()
+    
+    if search_value == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects\
+            .filter(show=True)\
+            .filter(
+                Q(first_name__icontains=search_value) |
+                Q(last_name__icontains=search_value) |
+                Q(email__icontains=search_value) |
+                Q(phone__icontains=search_value)
+            )\
+            .order_by('-id')
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '  
     }
     
     return render(
@@ -23,7 +53,8 @@ def contact(request, id):
     )
     
     context = {
-        'contact': single_contact  
+        'contact': single_contact,
+        'site_title': f'{single_contact.first_name} {single_contact.last_name} - '    
     }
     
     return render(
