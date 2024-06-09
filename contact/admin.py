4be02767695be6fa19cd0fc7e78e70758574e631
guugimeness <gucrome1@gmail.com>
2024-06-09 15:19:50 -0300

    feat: views: contacts + single contact

--- contact/admin.py
+++ contact/admin.py
@@ -5,13 +5,13 @@ from contact import models
 # Using decorator
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'category', 'owner',
+    list_display = 'id', 'first_name', 'last_name', 'category', 'show',
     ordering = '-id',
     list_filter = 'creation_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 100
-    list_editable = 'first_name', 'last_name'
+    list_editable = 'first_name', 'last_name', 'show',
     list_display_links = 'id',
 
 
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from contact.models import Contact
+from django.http import Http404
 
 def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    
+    context = {
+        'contacts': contacts    
+    }
+    
     return render(
         request,
-        'contact/index.html'
+        'contact/index.html',
+        context,
+    )
+
+def contact(request, id):
+    single_contact = get_object_or_404(
+        Contact.objects,
+        pk=id,
+        show=True
+    )
+    
+    context = {
+        'contact': single_contact  
+    }
+    
+    return render(
+        request,
+        'contact/contact.html',
+        context,
     )
